78acce5dcad29dac9f2a9d61e1bdf3311c9104f9
VO7ice <Vo7ice@outlook.com>
2016-11-14 21:14:23 +0800

    hero和good已抓取简单信息

--- UUU9/uuu9.py
+++ UUU9/uuu9.py
@@ -1,17 +1,19 @@
 # coding=utf-8
+import leancloud
+
 __author__ = 'Vo7ice'
 from bs4 import BeautifulSoup
 import requests, re
 import time
 import sys
+from leancloud import Object
 
 
 class Item:
     def __init__(self):
-        self.pageIndex = 1
+        leancloud.init("tsVy6mtJMvQ1zhpnY4wdTVXo-gzGzoHsz", "gnJe9bgWGDfUaqaX5LQzyWHJ")
         self.user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
         self.headers = {'User-Agent': self.user_agent}
-        self.heroNames = []
 
         """
         <a href="/hero/show/Oracle">
@@ -26,30 +28,63 @@ class Item:
         print 'req status_code:%d' % req.status_code
         if req.status_code == 200:
             soup = BeautifulSoup(req.content, 'html.parser')
+            # i = 0
+            # type = sys.getfilesystemencoding()
+            # all = soup.find_all('div', 'picbox cl')
+            # print 'size:%d' % len(all)
+            herolist = soup.find('div', id='herolist1')
+            heroes = herolist.findAll('a')
             i = 0
-            type = sys.getfilesystemencoding()
-            list = soup.find_all('div', 'picbox cl')
-            print 'size:%d' % len(list)
-            for link in soup.find_all('div', 'picbox cl'):
-                print i
-                hero = Hero()
-                herolist = link.find_all('a')
-                print 'list:%d' % len(herolist)
-                hero_url = link.a['href']
-                if link.a.contents[1].has_attr("hid"):
-                    hero_id = link.a.contents[1]['hid']
-                else:
-                    good_id = link.a.contents[1]['gid']
-                hero_img = link.a.contents[1]['src']
-                hero_name = link.find('span')
-                print 'url:%s, id:%s, img:%s, name:%s' % (hero_url, hero_id, hero_img, hero_name)
+            try:
+                for hero in heroes:
+                    print i
+                    heroInfo = HeroSave()
+                    print 'href%s' % hero['href']
+                    heroInfo.set('hero_url', hero['href']).save()
+                    print 'hero_id%s' % hero.img['hid']
+                    heroInfo.set('hero_id', hero.img['hid']).save()
+                    print 'img_url%s' % hero.img['src']
+                    heroInfo.set('img_url', hero.img['src']).save()
+                    print 'name%s' % hero.span.string
+                    heroInfo.set('name',hero.span.string).save()
+                    i = i + 1
+            except leancloud.LeanCloudError as e:
+                print e.message
+        else:
+            print 'network error'
+
+    def getGoodList(self, url):
+        req = requests.get(url, headers=self.headers)
+        print 'req status_code:%d' % req.status_code
+        if req.status_code == 200:
+            soup = BeautifulSoup(req.content, 'html.parser')
+            goodlist = soup.find('div', id='goodslist1')
+            goods = goodlist.findAll('a')
+            i = 0
+            try:
+                for good in goods:
+                    print i
+                    goodInfo = GoodSave()
+                    print 'href%s' % good['href']
+                    goodInfo.set('good_url', good['href']).save()
+                    print 'good_id%s' % good.img['gid']
+                    goodInfo.set('good_id', good.img['gid']).save()
+                    print 'img_url%s' % good.img['src']
+                    goodInfo.set('img_url', good.img['src']).save()
+                    print 'name%s' % good.span.string
+                    goodInfo.set('name',good.span.string).save()
+            except leancloud.LeanCloudError as e:
+                print e.message
+        else:
+            print 'network error'
 
     def start(self):
         baseUrl = "http://db.dota2.uuu9.com/"
-        self.getHeroList(baseUrl)
+        # self.getHeroList(baseUrl)
+        self.getGoodList(baseUrl)
 
 
-class Hero(object):
+class HeroSave(Object):
     @property
     def hero_url(self):
         return self.get('hero_url')
@@ -75,12 +110,47 @@ class Hero(object):
         return self.set('img_url', value)
 
     @property
-    def name(self):
-        return self.get('name')
+    def hero_name(self):
+        return self.get('hero_name')
 
-    @name.setter
-    def name(self, value):
-        return self.set('name', value)
+    @hero_name.setter
+    def hero_name(self, value):
+        return self.set('hero_name', value)
+
+
+class GoodSave(Object):
+    @property
+    def good_url(self):
+        return self.get('good_url')
+
+    @good_url.setter
+    def good_url(self, value):
+        return self.set('good_url', value)
+
+    @property
+    def good_id(self):
+        return self.get('good_id')
+
+    @good_id.setter
+    def good_id(self, value):
+        return self.set('good_id', value)
+
+    @property
+    def img_url(self):
+        return self.get('img_url')
+
+    @good_url.setter
+    def img_url(self, value):
+        return self.set('img_url', value)
+
+    @property
+    def good_name(self):
+        return self.get('good_name')
+
+    @good_name.setter
+    def good_name(self, value):
+        return self.set('good_name', value)
+
 
 item = Item()
 item.start()
